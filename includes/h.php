b35fd6c6cca33124354aae78c253ede9d3406ff7
shreyansh jain <jain.shreyansh0@gmail.com>
2018-03-18 23:16:23 +0530

    signin ok

--- includes/h.php
+++ includes/h.php
@@ -1,38 +1,39 @@
-
 <?php
+echo "asd";
+
+/*
 
 session_start();
 
 
 echo "1";
-if (isset($_POST['submit'])){
-	include 'dbh-inc.php';
-	$uid=mysqli_real_escape_string($conn,$_POST['uid']);
-	$pwd=mysqli_real_escape_string($conn,$_POST['pwd']);
+if (isset($_POST['submit']))
+//	include 'dbh-inc.php';
+//	$uid=mysqli_real_escape_string($conn,$_POST['uid']);
+//	$pwd=mysqli_real_escape_string($conn,$_POST['pwd']);
 echo 2;	
 	//error handlers!!
 	
-	if(empty($uid)||empty($pwd)){
-		header("Location: ../index.php?login=empty");
-		exit();
+//	if(empty($uid)||empty($pwd)){
+//		header("Location: ../index.php?login=empty");
+//		exit();
 		
 	}
 
-	else{
-		$sql="select * from users where user_uid='$uid'";
+//	else{
+//		$sql="select * from users where user_uid='$uid'";
 		$result=mysqli_query($conn,$sql);
-		$resultCheck=mysqli_num_rows($result);
+//		$resultCheck=mysqli_num_rows($result);
 		if($resultCheck<1)
 		{header("Location: ../index.php?login=error");
 		exit();
 		}
 
-		else{
-		//$row=mysqli_fetch_assoc($result);
-		if($row=mysqli_fetch_assoc($result))
+//		else{
+		if($row=mysqli_fetch_assoc($result)
 			{//dehashing pwd
 			$hashedPwdCheck=password_verify($pwd,$row['user_pwd']);
-				if($hashedPwdCheck==false){
+//				if($hashedPwdCheck==false){
 				header("Location: ../index.php?login=error");
 				exit();
 				}
@@ -41,18 +42,12 @@ echo 2;
 				$_SESSION['u_first']=$row['user_first'];
 				$_SESSION['u_last']=$row['user_last'];
 				$_SESSION['u_email']=$row['user_email'];
-				$_SESSION['u_uid']=$row['user_uid'];
+//				$_SESSION['u_uid']=$row['user_uid'];
 				header("Location: ../index.php?login=success");
 				exit();
 				
 				}
 			}
 		}	
-	}
-}
-
-else{
-header("Location: ../index.php?login=error");
-exit();
-}
-
+	}*/
+?>
